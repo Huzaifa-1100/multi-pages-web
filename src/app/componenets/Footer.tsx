9132a256cb5a4f6e29a8e4e73d23e73f3d8f19d0
Huzaifa-1100 <asadhussainshad@gmail.com>
2024-09-22 02:56:19 +0500

    update footer

--- src/app/componenets/Footer.tsx
+++ src/app/componenets/Footer.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { FaSquareGithub, FaSquareXTwitter } from "react-icons/fa6";
+import { FaSquareGithub } from "react-icons/fa6";
 import {
   FaFacebookSquare,
-  FaInstagramSquare,
   FaLinkedin,
 } from "react-icons/fa";
 
